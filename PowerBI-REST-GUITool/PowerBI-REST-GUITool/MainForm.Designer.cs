31c3878d1f5d7f08dc96e99814ec29f7dd07258d
root <ahkim@ahkim.com>
2017-03-15 16:27:19 +0800

    Complete basic features - Create/Get Datasets and Get Tables.

---
+++ PowerBI-REST-GUITool/PowerBI-REST-GUITool/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace PowerBI_REST_GUITool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPayload = new System.Windows.Forms.TextBox();
+            this.tbClientID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPUT = new System.Windows.Forms.Button();
+            this.btnPOST = new System.Windows.Forms.Button();
+            this.btnGETDatasets = new System.Windows.Forms.Button();
+            this.btnGetTables = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbPayload
+            // 
+            this.tbPayload.Location = new System.Drawing.Point(12, 37);
+            this.tbPayload.Multiline = true;
+            this.tbPayload.Name = "tbPayload";
+            this.tbPayload.Size = new System.Drawing.Size(804, 715);
+            this.tbPayload.TabIndex = 0;
+            // 
+            // tbClientID
+            // 
+            this.tbClientID.Location = new System.Drawing.Point(87, 9);
+            this.tbClientID.Name = "tbClientID";
+            this.tbClientID.Size = new System.Drawing.Size(288, 20);
+            this.tbClientID.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ClientID";
+            // 
+            // btnPUT
+            // 
+            this.btnPUT.Location = new System.Drawing.Point(381, 7);
+            this.btnPUT.Name = "btnPUT";
+            this.btnPUT.Size = new System.Drawing.Size(75, 23);
+            this.btnPUT.TabIndex = 4;
+            this.btnPUT.Text = "PUT";
+            this.btnPUT.UseVisualStyleBackColor = true;
+            this.btnPUT.Click += new System.EventHandler(this.btnPUT_Click);
+            // 
+            // btnPOST
+            // 
+            this.btnPOST.Location = new System.Drawing.Point(462, 7);
+            this.btnPOST.Name = "btnPOST";
+            this.btnPOST.Size = new System.Drawing.Size(75, 23);
+            this.btnPOST.TabIndex = 5;
+            this.btnPOST.Text = "POST";
+            this.btnPOST.UseVisualStyleBackColor = true;
+            this.btnPOST.Click += new System.EventHandler(this.btnPOST_Click);
+            // 
+            // btnGETDatasets
+            // 
+            this.btnGETDatasets.Location = new System.Drawing.Point(543, 7);
+            this.btnGETDatasets.Name = "btnGETDatasets";
+            this.btnGETDatasets.Size = new System.Drawing.Size(92, 23);
+            this.btnGETDatasets.TabIndex = 6;
+            this.btnGETDatasets.Text = "GET Datasets";
+            this.btnGETDatasets.UseVisualStyleBackColor = true;
+            this.btnGETDatasets.Click += new System.EventHandler(this.btnGETDatasets_Click);
+            // 
+            // btnGetTables
+            // 
+            this.btnGetTables.Location = new System.Drawing.Point(641, 7);
+            this.btnGetTables.Name = "btnGetTables";
+            this.btnGetTables.Size = new System.Drawing.Size(92, 23);
+            this.btnGetTables.TabIndex = 7;
+            this.btnGetTables.Text = "GET Tables";
+            this.btnGetTables.UseVisualStyleBackColor = true;
+            this.btnGetTables.Click += new System.EventHandler(this.btnGetTables_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 764);
+            this.Controls.Add(this.btnGetTables);
+            this.Controls.Add(this.btnGETDatasets);
+            this.Controls.Add(this.btnPOST);
+            this.Controls.Add(this.btnPUT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbClientID);
+            this.Controls.Add(this.tbPayload);
+            this.Name = "MainForm";
+            this.Text = "PowerBI REST Call";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPayload;
+        private System.Windows.Forms.TextBox tbClientID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPUT;
+        private System.Windows.Forms.Button btnPOST;
+        private System.Windows.Forms.Button btnGETDatasets;
+        private System.Windows.Forms.Button btnGetTables;
+    }
+}
+
---
+++ PowerBI-REST-GUITool/PowerBI-REST-GUITool/MainForm.cs
@@ -0,0 +1,298 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace PowerBI_REST_GUITool
+{
+    public partial class MainForm : Form
+    {
+        //Step 1 - Replace {client id} with your client app ID. 
+        //To learn how to get a client app ID, see Register a client app (https://msdn.microsoft.com/en-US/library/dn877542.aspx#clientID)
+        private static string clientID = string.Empty;
+
+        //RedirectUri you used when you registered your app.
+        //For a client app, a redirect uri gives AAD more details on the specific application that it will authenticate.
+        private static string redirectUri = "https://login.live.com/oauth20_desktop.srf";
+
+        //Resource Uri for Power BI API
+        private static string resourceUri = Properties.Settings.Default.PowerBiAPI;
+
+        //OAuth2 authority Uri
+        private static string authority = Properties.Settings.Default.AADAuthorityUri;
+
+        private static AuthenticationContext authContext = null;
+        private static string token = String.Empty;
+
+        //Uri for Power BI datasets
+        private static string datasetsUri = Properties.Settings.Default.PowerBiDataset;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPUT_Click(object sender, EventArgs e)
+        {
+            if (clientID == string.Empty)
+            {
+                clientID = tbClientID.Text;
+                tbClientID.Enabled = false;
+            }
+                
+            // PUT
+            tbPayload.Text = UpdateDataset(tbPayload.Text);
+        }
+
+        private void btnPOST_Click(object sender, EventArgs e)
+        {
+            if (clientID == string.Empty)
+            {
+                clientID = tbClientID.Text;
+                tbClientID.Enabled = false;
+            }
+
+            // POST
+            tbPayload.Text = CreateDataset(tbPayload.Text);
+        }
+
+        private void btnGETDatasets_Click(object sender, EventArgs e)
+        {
+            if (clientID == string.Empty)
+            {
+                clientID = tbClientID.Text;
+                tbClientID.Enabled = false;
+            }
+
+            // GET
+            tbPayload.Text = GetDatasetsString();
+        }
+
+        private void btnGetTables_Click(object sender, EventArgs e)
+        {
+            if (clientID == string.Empty)
+            {
+                clientID = tbClientID.Text;
+                tbClientID.Enabled = false;
+            }
+
+            // GET
+            //Get a dataset id from a Dataset name. The dataset id is used for UpdateTableSchema, AddRows, and DeleteRows
+            string datasetId = ((GetDatasets().value).GetDataset(tbPayload.Text)).Id;
+
+            tbPayload.Text = GetTablesString(datasetId);
+
+            //table[] tables = GetTables(datasetId).value;
+            //StringBuilder sb = new StringBuilder();
+            //foreach (table table in tables)
+            //{
+            //    sb.AppendLine(String.Format("Name: {0}", table.Name));
+            //}
+
+            //tbPayload.Text = sb.ToString();
+        }
+
+        //The Create Dataset operation creates a new Dataset from a JSON schema definition and returns the Dataset ID 
+        //and the properties of the dataset created.
+        //POST https://api.powerbi.com/v1.0/myorg/datasets
+        //Create Dataset operation: https://msdn.microsoft.com/en-US/library/mt203562(Azure.100).aspx
+        static string CreateDataset(string jsonText)
+        {
+            //In a production application, use more specific exception handling.           
+            //Create a POST web request
+            HttpWebRequest request = DatasetRequest(String.Format("{0}/datasets?defaultRetentionPolicy=basicFIFO", datasetsUri), "POST", AccessToken());
+
+            //POST request using the json schema from Product
+            return PostRequest(request, jsonText);
+        }
+
+        //The Create Dataset operation creates a new Dataset from a JSON schema definition and returns the Dataset ID 
+        //and the properties of the dataset created.
+        //POST https://api.powerbi.com/v1.0/myorg/datasets
+        //Create Dataset operation: https://msdn.microsoft.com/en-US/library/mt203562(Azure.100).aspx
+        static string UpdateDataset(string jsonText)
+        {
+            //In a production application, use more specific exception handling.           
+            //Create a POST web request
+            HttpWebRequest request = DatasetRequest(String.Format("{0}/datasets", datasetsUri), "PUT", AccessToken());
+
+            //POST request using the json schema from Product
+            return PostRequest(request, jsonText);
+        }
+
+        //The Get Datasets operation returns a JSON list of all Dataset objects that includes a name and id.
+        //GET https://api.powerbi.com/v1.0/myorg/datasets
+        //Get Dataset operation: https://msdn.microsoft.com/en-US/library/mt203567.aspx
+        static string GetDatasetsString()
+        {
+            //Create a GET web request to list all datasets
+            HttpWebRequest request = DatasetRequest(String.Format("{0}/datasets", datasetsUri), "GET", AccessToken());
+
+            //Get HttpWebResponse from GET request
+            string responseContent = GetResponse(request);
+
+            return responseContent;
+        }
+
+        //The Get Datasets operation returns a JSON list of all Dataset objects that includes a name and id.
+        //GET https://api.powerbi.com/v1.0/myorg/datasets
+        //Get Dataset operation: https://msdn.microsoft.com/en-US/library/mt203567.aspx
+        static Datasets GetDatasets()
+        {
+            Datasets response = null;
+
+            //Create a GET web request to list all datasets
+            HttpWebRequest request = DatasetRequest(String.Format("{0}/datasets", datasetsUri), "GET", AccessToken());
+
+            //Get HttpWebResponse from GET request
+            string responseContent = GetResponse(request);
+
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            response = (Datasets)json.Deserialize(responseContent, typeof(Datasets));
+
+            return response;
+        }
+
+        //The Get Tables operation returns a JSON list of Tables for the specified Dataset.
+        //GET https://api.powerbi.com/v1.0/myorg/datasets/{dataset_id}/tables
+        //Get Tables operation: https://msdn.microsoft.com/en-US/library/mt203556.aspx
+        static string GetTablesString(string datasetId)
+        {
+            //Create a GET web request to list all datasets
+            HttpWebRequest request = DatasetRequest(String.Format("{0}/datasets/{1}/tables", datasetsUri, datasetId), "GET", AccessToken());
+
+            //Get HttpWebResponse from GET request
+            string responseContent = GetResponse(request);
+            
+            return responseContent;
+        }
+
+        //The Get Tables operation returns a JSON list of Tables for the specified Dataset.
+        //GET https://api.powerbi.com/v1.0/myorg/datasets/{dataset_id}/tables
+        //Get Tables operation: https://msdn.microsoft.com/en-US/library/mt203556.aspx
+        static Tables GetTables(string datasetId)
+        {
+            Tables response = null;
+
+            //Create a GET web request to list all datasets
+            HttpWebRequest request = DatasetRequest(String.Format("{0}/datasets/{1}/tables", datasetsUri, datasetId), "GET", AccessToken());
+
+            //Get HttpWebResponse from GET request
+            string responseContent = GetResponse(request);
+
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            response = (Tables)json.Deserialize(responseContent, typeof(Tables));
+
+            return response;
+        }
+
+        private static HttpWebRequest DatasetRequest(string datasetsUri, string method, string accessToken)
+        {
+            HttpWebRequest request = System.Net.WebRequest.Create(datasetsUri) as System.Net.HttpWebRequest;
+            request.KeepAlive = true;
+            request.Method = method;
+            request.ContentLength = 0;
+            request.ContentType = "application/json";
+            request.Headers.Add("Authorization", String.Format("Bearer {0}", accessToken));
+
+            return request;
+        }
+
+        /// <summary>
+        /// Use AuthenticationContext to get an access token
+        /// </summary>
+        /// <returns></returns>
+        static string AccessToken()
+        {
+            if (token == String.Empty)
+            {
+                //Get Azure access token
+                // Create an instance of TokenCache to cache the access token
+                TokenCache TC = new TokenCache();
+                // Create an instance of AuthenticationContext to acquire an Azure access token
+                authContext = new AuthenticationContext(authority, TC);
+                // Call AcquireToken to get an Azure token from Azure Active Directory token issuance endpoint
+                token = authContext.AcquireToken(resourceUri, clientID, new Uri(redirectUri), PromptBehavior.Auto).AccessToken;
+            }
+            else
+            {
+                // Get the token in the cache
+                token = authContext.AcquireTokenSilent(resourceUri, clientID).AccessToken;
+            }
+
+            return token;
+        }
+
+        private static string GetResponse(HttpWebRequest request)
+        {
+            string response = string.Empty;
+
+            using (HttpWebResponse httpResponse = request.GetResponse() as System.Net.HttpWebResponse)
+            {
+                //Get StreamReader that holds the response stream
+                using (StreamReader reader = new System.IO.StreamReader(httpResponse.GetResponseStream()))
+                {
+                    response = reader.ReadToEnd();
+                }
+            }
+
+            return response;
+        }
+
+        private static string PostRequest(HttpWebRequest request, string json)
+        {
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(json);
+            request.ContentLength = byteArray.Length;
+
+            //Write JSON byte[] into a Stream
+            using (Stream writer = request.GetRequestStream())
+            {
+                writer.Write(byteArray, 0, byteArray.Length);
+            }
+
+            return GetResponse(request);
+        }
+
+    }
+
+    public class Datasets
+    {
+        public dataset[] value { get; set; }
+    }
+
+    public class dataset
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Tables
+    {
+        public table[] value { get; set; }
+    }
+
+    public class table
+    {
+        public string Name { get; set; }
+    }
+
+    public class Groups
+    {
+        public group[] value { get; set; }
+    }
+
+    public class group
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
